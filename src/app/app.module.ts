a204e7ac1f015e0e57bb7c11428f2b3bc86fd6d2
francisco Mella <francisco.mella.torres@outlook.com>
2018-10-16 15:31:34 +1200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TheQuoteComponent } from './the-quote/the-quote.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TheQuoteComponent
   ],
   imports: [
     BrowserModule
